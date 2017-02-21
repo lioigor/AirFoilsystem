ced35d990cf77c4096eb664c48cc9b3513ab0586
lio <lio@77.47.208.39>
2017-02-21 19:00:22 +0200

    save 5.6

--- SpringMVCAIRFoilsystem10/src/main/java/com/lioigor22/controllers/AutorizationController.java
+++ SpringMVCAIRFoilsystem10/src/main/java/com/lioigor22/controllers/AutorizationController.java
@@ -1,105 +1,72 @@
 package com.lioigor22.controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+
+import com.lioigor22.objects.User;
+import com.lioigor22.service.SecurityService;
+import com.lioigor22.service.UserService;
+import com.lioigor22.validators.UserValidator;
 
 @Controller
 public class AutorizationController {
 
-	private static final Logger logger = LoggerFactory.getLogger(AutorizationController.class);
+	@Autowired
+	private UserService userService;
 
-	@RequestMapping(value = { "/", "/login" }, method = RequestMethod.GET)
-	public ModelAndView login(@RequestParam(value = "error", required = false) String error) {
+	@Autowired
+	private SecurityService securityService;
 
-		ModelAndView model = new ModelAndView();
-		if (error != null) {
-			model.addObject("error", "Invalid username or password!");
-		}
+	@Autowired
+	private UserValidator userValidator;
 
-		model.setViewName("login");
+	@RequestMapping(value = "/registration", method = RequestMethod.GET)
+	public String registration(Model model) {
+		model.addAttribute("userForm", new User());
 
-		return model;
-
+		return "registration";
 	}
 
-	// private UserService userService;
-	//
-	// @RequestMapping(value = { "/login" }, method = RequestMethod.GET)
-	// public String home(Model model) {
-	//
-	// model.addAttribute("user", new User());
-	//
-	// return "login";
-	// }
-	//
-	// @RequestMapping(value = { "/register" }, method = RequestMethod.GET)
-	// public String registry(Model model) {
-	//
-	// model.addAttribute("user", new User());
-	//
-	// return "register";
-	// }
-	//
-	// @Autowired(required = true)
-	// @Qualifier(value = "userService")
-	// public void setUserService(UserService ps) {
-	// this.userService = ps;
-	// }
-	//
-	// @RequestMapping(value = "/login/users", method = RequestMethod.GET)
-	// public String listUsers(Model model) {
-	// model.addAttribute("user", new User());
-	// model.addAttribute("listUsers", this.userService.listUsers());
-	// return "user";
-	// }
-	//
-	// // For add and update user both
-	// @RequestMapping(value = "/login/add", method = RequestMethod.POST)
-	// public String addUser(@ModelAttribute("user") User p, Model model) {
-	//
-	// boolean check = this.userService.checkLogin(p);
-	// model.addAttribute("check", check);
-	//
-	// if (check)
-	// return "register";
-	// else {
-	// this.userService.addUser(p);
-	// model.addAttribute("regsuccess", true);
-	//
-	// return "home";
-	// }
-	//
-	// }
-	//
-	// @RequestMapping("/login/remove/{id}")
-	// public String removeUser(@PathVariable("id") int id) {
-	//
-	// this.userService.removeUser(id);
-	// return "redirect:/users";
-	// }
-	//
-	// @RequestMapping("/login/edit/{id}")
-	// public String editUser(@PathVariable("id") int id, Model model) {
-	// model.addAttribute("user", this.userService.getUserById(id));
-	// model.addAttribute("listUsers", this.userService.listUsers());
-	// return "user";
-	// }
-	//
-	// @RequestMapping(value = "/login/check", method = RequestMethod.POST)
-	// public String checkUser(@ModelAttribute("user") User user, Model model) {
-	//
-	// boolean check = this.userService.checkUser(user);
-	// model.addAttribute("check", check);
-	// if (check)
-	// return "home";
-	// else
-	// model.addAttribute("check", true);
-	// return "login";
-	// }
+	@RequestMapping(value = "/registration", method = RequestMethod.POST)
+	public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult, Model model) {
+		userValidator.validate(userForm, bindingResult);
 
+		if (bindingResult.hasErrors()) {
+			return "registration";
+		}
+
+		userService.save(userForm);
+
+		securityService.autoLogin(userForm.getUsername(), userForm.getConfirmPassword());
+
+		return "redirect:/welcome";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(Model model, String error, String logout) {
+		if (error != null) {
+			model.addAttribute("error", "Username or password is incorrect.");
+		}
+
+		if (logout != null) {
+			model.addAttribute("message", "Logged out successfully.");
+		}
+
+		return "login";
+	}
+
+	@RequestMapping(value = { "/", "/welcome" }, method = RequestMethod.GET)
+	public String welcome(Model model) {
+		return "welcome";
+	}
+
+	@RequestMapping(value = "/admin", method = RequestMethod.GET)
+	public String admin(Model model) {
+		return "admin";
+	}
 }
